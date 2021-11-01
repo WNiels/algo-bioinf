441b69f8a351a37b0fa6fca117fab3a2db0be6df
Niels Westphal <commit@nielswestphal.de>
2021-11-01 12:51:36 +0100

    scs proof

---
+++ recursion_viz.py
@@ -0,0 +1,33 @@
+import functools
+import graphviz
+
+def recursion_visualizer(func):
+    func_name = func.__name__
+    separator = '|  '
+    recursion_visualizer.recursion_depth = 0
+    recursion_visualizer.call_stack = [] # Store Nodes here
+    recursion_visualizer.graph = graphviz.Digraph()
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+
+        print(f'{separator * recursion_visualizer.recursion_depth}|-- {func_name}({", ".join(map(str, args))})')
+
+        node = f'{recursion_visualizer.recursion_depth}_{func_name}({", ".join(map(str, args))})'
+        recursion_visualizer.graph.node(node, node)
+
+        recursion_visualizer.recursion_depth += 1
+
+        if len(recursion_visualizer.call_stack) > 0:
+            recursion_visualizer.graph.edge(recursion_visualizer.call_stack[-1], node)
+
+        recursion_visualizer.call_stack.append(node)
+        result = func(*args, **kwargs)
+
+        recursion_visualizer.graph.node(node, label=node+' -> '+result)
+
+        recursion_visualizer.recursion_depth -= 1
+        recursion_visualizer.call_stack.pop()
+        print(f'{separator * (recursion_visualizer.recursion_depth + 1)}|-- return {result}')
+        return result
+    return wrapper
+
---
+++ scs.py
@@ -0,0 +1,31 @@
+from recursion_viz import recursion_visualizer as rv
+ 
+@rv
+def SCS_DP(s: str, t: str, lookup: dict = {}) -> str:
+    if len(s) == 0 or len(t) == 0:  
+        return s+t
+
+    key = (s,t)
+
+    if not key in lookup:                 
+        if s[-1] == t[-1]:              
+            lookup[key] = SCS_DP(s=s[0:-1], t=t[0:-1], lookup=lookup) + s[-1]
+        else:
+            lookup[key] = min(SCS_DP(s[0:-1], t=t, lookup=lookup)+s[-1], SCS_DP(s=s, t=t[0:-1], lookup=lookup)+t[-1])
+
+    return lookup[key]
+
+# Call function
+s = "ABCBDAB"
+t = "BDCABA"
+scs = SCS_DP(s,t)
+
+print(rv.graph)
+
+rv.graph.render('test-output/round-table.gv', view=True)
+
+# print("Edges:")
+# print(RV.graph.get_edge_list())
+
+
+print(scs)
